66c8ffb06874bdd6f628b99d089afa3d68a2d439
zuzayka <bool6@yandex.ru>
2022-02-12 21:13:25 +0300

    HW1 реализовал методы save, get, delete, clear, getAll, size в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,61 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                System.out.println(storage[i].uuid);
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume dummy = new Resume();
+        dummy.uuid = "This uuid is not in the Resume";
+        for (int i = 0; i < 10000; i++) {
+            try {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
+            } catch (NullPointerException e) {
+                break;
+            }
+        }
+        return dummy;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < 10000; i++) {
+
+            if (uuid.equals(storage[i].uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, 9999 - i);
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        while (i < 10000) {
+            try {
+                storage[i].uuid.equals(null);
+            } catch (NullPointerException e) {
+                break;
+            }
+            i++;
+        }
+        return i;
     }
 }
