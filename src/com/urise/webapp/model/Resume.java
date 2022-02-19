02e136f7c104392f042831634de06b77df4c4e01
zuzayka <bool6@yandex.ru>
2022-02-19 20:18:26 +0300

    Update all packages

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -4,9 +4,16 @@ package com.urise.webapp.model;
  * Initial resume class
  */
 public class Resume {
-
     // Unique identifier
-    String uuid;
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 
     @Override
     public String toString() {
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -10,38 +10,42 @@ import java.util.Arrays;
 public class ArrayStorage {
     private int size = 0;
 
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
-    int getSize() {
+    public int getSize() {
         return size;
     }
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void update(Resume r) {
+
+    }
+
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
-        if (uuid.equals(storage[size - 1].uuid)) {
+    public void delete(String uuid) {
+        if (uuid.equals(storage[size - 1].getUuid())) {
             storage[size - 1] = null;
             size--;
         }
         for (int i = 0; i < size - 1; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 System.arraycopy(storage, i + 1, storage, i, size - i - 1);
                 storage[size - 1] = null;
                 size--;
@@ -53,7 +57,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 }
