1f5c3dd089b45a9af1c75c285bbe976f38e33682
zuzayka <bool6@yandex.ru>
2022-02-27 21:18:58 +0300

    Introduce Interface

---
+++ src/com/urise/webapp/storage/Storage.java
@@ -0,0 +1,25 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public interface Storage {
+    int getSize();
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll();
+}
