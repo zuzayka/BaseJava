09f0cf1d5ec66281a0e4b14cd41f1ffaa2359022
zuzayka <bool6@yandex.ru>
2022-03-14 20:42:36 +0300

    HW3 MainArray.class execution trouble (corrected)

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -16,40 +16,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         System.arraycopy(storage, index + 1, storage, index, size - index - 1);
     }
 
-  /*
-    @Override
-    public void save(Resume r) {
-        String uuid = r.getUuid();
-        if (size == STORAGE_LIMIT) {
-            System.out.println("Error: " + uuid + " is not saved");
-            return;
-        }
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            System.out.println("Error: " + uuid + " is not saved");
-            return;
-        }
-        index = -index - 1;
-        System.arraycopy(storage, index, storage, index + 1, size - index);
-        storage[index] = r;
-        size++;
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            if (index < size - 1) {
-                System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-            }
-            storage[size - 1] = null;
-            size--;
-            return;
-        }
-        System.out.println("Error: " + uuid + " is not deleted");
-    }
-    */
-
     @Override
     protected int findIndex(String uuid) {
         Resume searchKey = new Resume();
