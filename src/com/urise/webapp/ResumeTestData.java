e81ac37b23856e87a48b3024980cd244b8b7b4ec
zuzayka <bool6@yandex.ru>
2023-07-04 10:12:22 +0300

    HW8_commit_4(ResumeTestData)

--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -1,9 +1,8 @@
 package com.urise.webapp;
 
-import com.urise.webapp.model.ListSection;
-import com.urise.webapp.model.Resume;
-import com.urise.webapp.model.TextSection;
+import com.urise.webapp.model.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,52 +13,54 @@ public class ResumeTestData {
     public static Resume resumeFill(String uuid, String fullName) {
         Resume r = new Resume(uuid, fullName);
 
-//        final LocalDate startMFTIS = LocalDate.of(1984, 9, 1);
-//        final LocalDate stopMFTIS = LocalDate.of(1987, 6, 1);
-//        final String titleMFTIS = "";
-//        final String descriptionMFTIS = "Закончил с отличием";
-//        final Organization.Period periodMFTIS = new Organization.Period(startMFTIS, stopMFTIS, titleMFTIS, descriptionMFTIS);
-//        final String nameMFTIS = "Заочная физико-техническая школа при МФТИ";
-//        final String webSiteMFTIS = "https://mipt.ru";
-//        final List<Organization.Period> listMFTIS = new ArrayList<>();
-//        listMFTIS.add(periodMFTIS);
-//        final Organization orgMFTIS = new Organization(nameMFTIS, webSiteMFTIS, listMFTIS);
-//
-//        final LocalDate startSPBU1 = LocalDate.of(1987, 9, 1);
-//        final LocalDate stopSPBU1 = LocalDate.of(1993, 7, 1);
-//        final String titleSPBU1 = "Инженер (программист Fortran, C)";
-//        final String descriptionSPBU1 = "";
-//        final Organization.Period periodSPBU1 = new Organization.Period(startSPBU1, stopSPBU1, titleSPBU1, descriptionSPBU1);
-//
-//        final LocalDate startSPBU2 = LocalDate.of(1993, 9, 1);
-//        final LocalDate stopSPBU2 = LocalDate.of(1996, 7, 1);
-//        final String titleSPBU2 = "Аспирантура (программист С, С++))";
-//        final String descriptionSPBU2 = "";
-//        final Organization.Period periodSPBU2 = new Organization.Period(startSPBU2, stopSPBU2, titleSPBU2, descriptionSPBU2);
-//
-//        final String nameSPBU = "Санкт-Петербургский национальный исследовательский университет " + "информационных технологий, механики и оптики";
-//        final String webSiteSPBU = "http://www.ifmo.ru";
-//        final ArrayList<Organization.Period> listSPBU = new ArrayList<>();
-//        listSPBU.add(periodSPBU1);
-//        listSPBU.add(periodSPBU2);
-//        final Organization orgSPBU = new Organization(nameSPBU, webSiteSPBU, listSPBU);
-//
-//        final LocalDate startAlcatel = LocalDate.of(1997, 9, 1);
-//        final LocalDate stopAlcatel = LocalDate.of(2005, 1, 1);
-//        final String titleAlcatel = "Инженер по аппаратному и программному тестированию";
-//        final String descriptionAlcatel = "";
-//        final Organization.Period periodAlcatel = new Organization.Period(startAlcatel, stopAlcatel, titleAlcatel, descriptionAlcatel);
-//        final String nameAlcatel = "Alcatel";
-//        final String webSiteAlcatel = "http://www.alcatel.ru";
-//        final List<Organization.Period> listAlcatel = new ArrayList<>();
-//        listAlcatel.add(periodAlcatel);
-//        final Organization orgAlcatel = new Organization(nameAlcatel, webSiteAlcatel, listAlcatel);
-//        System.out.println(orgAlcatel);
-//        List<Organization> experienceList = new ArrayList<>();
-//        experienceList.add(orgAlcatel);
-//        List<Organization> educationList = new ArrayList<>();
-//        educationList.add(orgMFTIS);
-//        educationList.add(orgSPBU);
+        final LocalDate startMFTIS = LocalDate.of(1984, 9, 1);
+        final LocalDate stopMFTIS = LocalDate.of(1987, 6, 1);
+        final String titleMFTIS = "";
+        final String descriptionMFTIS = "Закончил с отличием";
+        final Organization.Period periodMFTIS = new Organization.Period(startMFTIS, stopMFTIS, titleMFTIS, descriptionMFTIS);
+        final String nameMFTIS = "Заочная физико-техническая школа при МФТИ";
+        final String webSiteMFTIS = "https://mipt.ru";
+        final List<Organization.Period> listMFTIS = new ArrayList<>();
+        listMFTIS.add(periodMFTIS);
+        final Organization orgMFTIS = new Organization(nameMFTIS, webSiteMFTIS, listMFTIS);
+
+        final LocalDate startSPBU1 = LocalDate.of(1987, 9, 1);
+        final LocalDate stopSPBU1 = LocalDate.of(1993, 7, 1);
+        final String titleSPBU1 = "Инженер (программист Fortran, C)";
+        final String descriptionSPBU1 = "";
+        final Organization.Period periodSPBU1 = new Organization.Period(startSPBU1, stopSPBU1, titleSPBU1, descriptionSPBU1);
+
+        final LocalDate startSPBU2 = LocalDate.of(1993, 9, 1);
+        final LocalDate stopSPBU2 = LocalDate.of(1996, 7, 1);
+        final String titleSPBU2 = "Аспирантура (программист С, С++))";
+        final String descriptionSPBU2 = "";
+        final Organization.Period periodSPBU2 = new Organization.Period(startSPBU2, stopSPBU2, titleSPBU2, descriptionSPBU2);
+
+        final String nameSPBU = "Санкт-Петербургский национальный исследовательский университет " + "информационных технологий, механики и оптики";
+        final String webSiteSPBU = "http://www.ifmo.ru";
+        final ArrayList<Organization.Period> listSPBU = new ArrayList<>();
+        listSPBU.add(periodSPBU1);
+        listSPBU.add(periodSPBU2);
+        final Organization orgSPBU = new Organization(nameSPBU, webSiteSPBU, listSPBU);
+        List<Organization> educationList = new ArrayList<>();
+        educationList.add(orgMFTIS);
+        educationList.add(orgSPBU);
+        r.getSectionType().put(EDUCATION, new OrganizationSection(educationList));
+
+        final LocalDate startAlcatel = LocalDate.of(1997, 9, 1);
+        final LocalDate stopAlcatel = LocalDate.of(2005, 1, 1);
+        final String titleAlcatel = "Инженер по аппаратному и программному тестированию";
+        final String descriptionAlcatel = "";
+        final Organization.Period periodAlcatel = new Organization.Period(startAlcatel, stopAlcatel, titleAlcatel, descriptionAlcatel);
+        final String nameAlcatel = "Alcatel";
+        final String webSiteAlcatel = "http://www.alcatel.ru";
+        final List<Organization.Period> listAlcatel = new ArrayList<>();
+        listAlcatel.add(periodAlcatel);
+        final Organization orgAlcatel = new Organization(nameAlcatel, webSiteAlcatel, listAlcatel);
+        System.out.println(orgAlcatel);
+        List<Organization> experienceList = new ArrayList<>();
+        experienceList.add(orgAlcatel);
+        r.getSectionType().put(EXPERIENCE, new OrganizationSection(experienceList));
 
         final List<String> achievementList = new ArrayList<>() {{
         add("Организация команды и успешная реализация Java проектов для сторонних заказчиков: " + "приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей " + "спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + " + "Vaadin проект для комплексных DIY смет");
@@ -82,8 +83,6 @@ public class ResumeTestData {
 
         r.getSectionType().put(PERSONAL, new TextSection("Аналитический склад ума, сильная логика, креативность," + " инициативность. Пурист кода и архитектуры"));
         r.getSectionType().put(OBJECTIVE, new TextSection("Ведущий стажировок и корпоративного обучения по Java Web" + " и Enterprise технологиям"));
-//        r.getSectionType().put(EXPERIENCE, new OrganizationSection(experienceList));
-//        r.getSectionType().put(EDUCATION, new OrganizationSection(educationList));
         return r;
     }
 
