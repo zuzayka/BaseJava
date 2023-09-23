aab05c87e32f85516c786b1f187ce9d4116e38c4
zuzayka <bool6@yandex.ru>
2023-09-24 00:16:31 +0300

    HW12 commit_1(streams in StreamExamples.java)

---
+++ src/com/urise/StreamExamples.java
@@ -0,0 +1,22 @@
+package com.urise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExamples {
+    public int minValue(int[] values) {
+        return Integer.parseInt(Arrays.stream(values).sorted().distinct()
+                .mapToObj(String::valueOf).collect(Collectors.joining()));
+    }
+
+    public List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream().filter(e -> {
+            if (integers.stream().reduce(0, Integer::sum) % 2 == 0) {
+                return e % 2 == 0;
+            } else {
+                return e % 2 == 1;
+            }
+        }).collect(Collectors.toList());
+    }
+}
--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,28 +1,40 @@
 package com.urise.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     public static int counter;
-    private static final Object LOCK = new Object();
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+    //    private static final Object LOCK = new Object();
+    private static final Lock lock = new ReentrantLock();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
 
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         System.out.println(Thread.currentThread().getName());
 
         Thread thread0 = new Thread() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName());
+                System.out.println(Thread.currentThread().getState() + " " + Thread.currentThread().getName());
                 System.out.println(getName());
-                throw new IllegalStateException();
+//                throw new IllegalStateException();
             }
         };
-        System.out.println(thread0.getState());
+        System.out.println(thread0.getState() + " " + Thread.currentThread().getName());
         thread0.setDaemon(true);
         thread0.start();
-        System.out.println(thread0.getState());
+        System.out.println(thread0.getState() + " " + Thread.currentThread().getName());
 //        new Thread(new Runnable() {                                        // Это без лямбда
 //            @Override
 //            public void run() {
@@ -31,19 +43,46 @@ public class MainConcurrency {
 //    }).start();
 
 //      То же самое. Неявная запись Thread c интерфейсом Runnable с помощью лямбда:
+
+
         new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
-        System.out.println(thread0.getState());
+        System.out.println(thread0.getState() + " " + Thread.currentThread().getName());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);  // используется для
+//        получения завершенной нити и получения результата с этой нити
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Future<?> future = executorService.submit(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+//                    System.out.println(threadLocal.get().format(new Date()));
+                }
+                latch.countDown();
+                return 5;
+            });
+        }
+        executorService.shutdown();
+        latch.await(10, TimeUnit.SECONDS);
+
+
+/*      Вместо создания отдельных потоков используем executorService.submit(()
         for (int i = 0; i < THREADS_NUMBER; i++) {
             Thread thread = new Thread(() -> {
-
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
                 }
+                latch.countDown();
+
+
 //                System.out.println(counter+ " " + Thread.currentThread().getName());
             });
+*/
+
+
+/*  в лекции 12 заменяем на latch.
             thread.start();
             threads.add(thread);
             threads.forEach(t -> {
@@ -53,17 +92,32 @@ public class MainConcurrency {
                     e.printStackTrace();
                 }
             });
+*/
 //          При использовании .join() идет работа в один поток с ожиданием завершения потока начатого командой .start()
 //            thread.join();
-        }
+//        }
+
 //        Thread.sleep(500);
 
-        System.out.println(counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
+//        System.out.println(counter);   теперь atomic...
     }
 
-    private synchronized void inc() {
+    //    private synchronized void inc() {   //  убрали synchronized когда ввели lock
+    private void inc() {
 //        synchronized (this) {     // Если не использовать synchronized снаружи,
-        counter++;
+
+        atomicCounter.incrementAndGet();
+
+/*     если использовать AtomicInteger lock, try  не нужны
+       lock.lock();
+        try {
+            counter++;
+        } finally {
+            lock.unlock();
+        }*/
+
+
 //        }
     }
 }
