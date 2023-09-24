94e87fb4a9cba6ccc5b098e038e9c9855216d2b0
zuzayka <bool6@yandex.ru>
2023-09-24 17:36:31 +0300

    HW12 commit_2(exclude String in minValue, split oddOrEven)

--- src/com/urise/StreamExamples.java
+++ src/com/urise/StreamExamples.java
@@ -6,13 +6,14 @@ import java.util.stream.Collectors;
 
 public class StreamExamples {
     public int minValue(int[] values) {
-        return Integer.parseInt(Arrays.stream(values).sorted().distinct()
-                .mapToObj(String::valueOf).collect(Collectors.joining()));
+        return Arrays.stream(values).sorted().distinct()
+                .reduce(0, (base, element) -> base * 10 + element);
     }
 
     public List<Integer> oddOrEven(List<Integer> integers) {
+        boolean even = integers.stream().reduce(0, Integer::sum) % 2 == 0;
         return integers.stream().filter(e -> {
-            if (integers.stream().reduce(0, Integer::sum) % 2 == 0) {
+            if (even) {
                 return e % 2 == 0;
             } else {
                 return e % 2 == 1;
